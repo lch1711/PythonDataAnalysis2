ff774bf125e1f7843f2900689a1089e8c546b66a
HelloDataScience <87591273+HelloDataScience@users.noreply.github.com>
2023-04-19 16:48:34 +0900

    Add files via upload

---
+++ live/GraphicSetting.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# rc: runtime configuration
+plt.rc(group = 'font', family = 'Gowun Dodum', size = 10)
+plt.rc(group = 'figure', figsize = (4, 4), dpi = 100)
+plt.rc(group = 'axes', unicode_minus = False)
+plt.rc(group = 'legend', frameon = True, fc = '1', ec = '0')
